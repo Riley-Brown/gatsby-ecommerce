9936ccf9555526e27efa6143baa19c4be656c375
Riley Brown <riley@riley.gg>
2019-08-25 01:04:29 -0400

    added gatsby create pages API to create dynamic single product pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,52 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
+    const productTemplate = path.resolve("src/components/Product/index.js")
+    resolve(
+      graphql(`
+        {
+          allContentfulProducts {
+            edges {
+              node {
+                id
+                itemName
+                itemPrice
+                itemDescription {
+                  itemDescription
+                }
+                slug
+                itemImage {
+                  fluid {
+                    src
+                  }
+                }
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        result.data.allContentfulProducts.edges.forEach(edge => {
+          createPage({
+            path: edge.node.slug,
+            component: productTemplate,
+            context: {
+              slug: edge.node.slug,
+            },
+          })
+        })
+        return
+      })
+    )
+  })
+}
