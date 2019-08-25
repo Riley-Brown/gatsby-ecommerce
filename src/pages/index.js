5cbbac44b9be92ede89ad67144a81c226bc82330
Riley Brown <riley@riley.gg>
2019-08-25 01:09:00 -0400

    removed unneeded code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
-import Testing from "../components/Testing"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
+import Header from "../components/Header"
 
 import Products from "../components/Products"
 
 const IndexPage = () => (
-  <Layout>
-    {/* <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-      <Testing />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> */}
-    <Products />
-  </Layout>
+  <>
+    {/* <Header /> */}
+    <Layout>
+      <Products />
+    </Layout>
+  </>
 )
 
 export default IndexPage
