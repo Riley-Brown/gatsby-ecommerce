4136d0b21b67c3467e4b88fa59594983b0231c07
Riley Brown <riley@riley.gg>
2019-08-25 23:34:19 -0400

    added carousel functionality to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,23 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useRef, useState, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import { StyledHeader } from "./StyledHeader"
+import { useCarousel } from "../../hooks/useCarousel"
 
-const Header = ({ siteTitle }) => {
+const Header = () => {
+  const carouselContainer = useRef()
+
   const images = useStaticQuery(graphql`
     query HeroImagesQuery {
       allContentfulProductsCarousel {
         edges {
           node {
+            id
             images {
-              fluid {
-                src
+              fluid(quality: 80, resizingBehavior: SCALE) {
+                ...GatsbyContentfulFluid
               }
             }
           }
@@ -21,23 +25,31 @@ const Header = ({ siteTitle }) => {
       }
     }
   `)
-  console.log(images.allContentfulProductsCarousel.edges[0].node)
+  // console.log(images)
+
+  const { node } = images.allContentfulProductsCarousel.edges[0]
+
+  const [index] = useCarousel({
+    data: node.images,
+    timeout: 4000,
+    element: carouselContainer,
+    defaultVisible: true,
+  })
+
   return (
     <StyledHeader>
-      {images.allContentfulProductsCarousel.edges[0].node.images.map(img => (
-        // <Img fluid={img.fluid} />
-        <img src={img.fluid.src} />
-      ))}
+      <div className="carousel-container" ref={carouselContainer}>
+        <div className="tag-line">
+          <h1>Lorem Ipsum</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
+            voluptate!
+          </p>
+        </div>
+        <Img fluid={node.images[index].fluid} key={node.id} />
+      </div>
     </StyledHeader>
   )
-}
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
 }
 
 export default Header
