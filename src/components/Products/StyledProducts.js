26e3a45fff05e725cbc9cdfd39cb9f71237f35f1
Riley Brown <riley@riley.gg>
2019-08-27 23:57:28 -0400

    styled products component

--- src/components/Products/StyledProducts.js
+++ src/components/Products/StyledProducts.js
@@ -1,15 +1,49 @@
 import styled from "styled-components"
 
 export const StyledProducts = styled.div`
-  color: red;
   display: flex;
   justify-content: space-between;
+  flex-wrap: wrap;
+  .link-wrapper {
+    flex: 0 0 48%;
+    flex-wrap: wrap;
+    display: flex;
+    text-decoration: none;
+  }
   .product {
-    flex: 0 0 30%;
+    display: flex;
     cursor: pointer;
+    border-radius: 15px;
+    transition: 300ms;
+    margin-bottom: 40px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 1px 6px;
+    &:hover {
+      transform: translateY(-20px);
+    }
     img {
-      border-radius: 10px;
-      width: 100%;
+      border-top-left-radius: 10px;
+      border-bottom-left-radius: 10px;
+      width: 50%;
+      height: 200px;
+      object-fit: cover;
+      object-position: center;
+      display: block;
+      margin: 0;
+    }
+    .product-info {
+      margin-left: 10px;
+      flex: 100%;
+      padding: 10px;
+      color: #222;
+      h1 {
+        font-size: 3rem;
+      }
+      p {
+        font-size: 1.9rem;
+      }
+      h3 {
+        font-size: 2.2rem;
+      }
     }
   }
 `
