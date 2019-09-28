e45bbb06d95bb069f3af74d65e3dccaac198f1cc
Riley Brown <riley@riley.gg>
2019-09-28 14:43:06 -0400

    import nav to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 // import SEO from "../components/seo"
 import Header from "../components/Header"
+import Nav from "../components/Nav"
 
 import Products from "../components/Products"
 
 const IndexPage = () => (
   <>
+    <Nav />
     <Layout>
       <Header />
       <Products />
