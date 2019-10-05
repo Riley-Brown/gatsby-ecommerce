7baee04bc9b193dfdf902f8b25adaa5b7da79cd5
Riley Brown <riley@riley.gg>
2019-10-05 18:23:11 -0400

    moved nav styles into nav styled component

---
+++ src/components/Nav/StyledNav.js
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+
+export const StyledNav = styled.nav`
+  background: #fff;
+  margin-bottom: 1.45rem;
+  position: sticky;
+  top: 0;
+  background: #fff;
+  z-index: 1;
+  box-shadow: 2px 4px 10px rgba(0, 0, 0, 0.2);
+  margin-bottom: 50px;
+  .nav-container {
+    margin: 0 auto;
+    max-width: 1400;
+    padding: 1.45rem 1.0875rem;
+    text-align: center;
+    span {
+      color: white;
+      text-decoration: none;
+      text-align: center;
+      margin: auto;
+      cursor: pointer;
+      img {
+        margin: 0;
+        height: 70px;
+      }
+    }
+  }
+`
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,44 +1,17 @@
 import React from "react"
-import logo from "../../assets/img/rawberri-logo-cropped-bg.png"
-import { Link } from "gatsby"
+import { animateScroll } from "react-scroll"
 
-import { animateScroll } from "react-scroll/"
+import logo from "assets/img/rawberri-logo-cropped-bg.png"
+import { StyledNav } from "./StyledNav"
 
 export default function Nav() {
   return (
-    <nav
-      style={{
-        background: `#fff`,
-        marginBottom: `1.45rem`,
-        position: "sticky",
-        top: "0",
-        background: "#fff",
-        zIndex: "1",
-        boxShadow: "2px 4px 10px rgba(0,0,0,0.2)",
-        marginBottom: "50px",
-      }}
-    >
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 1400,
-          padding: `1.45rem 1.0875rem`,
-        }}
-      >
-        <span
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-            textAlign: "center",
-            margin: "auto",
-            display: "block",
-            cursor: "pointer",
-          }}
-          onClick={() => animateScroll.scrollToTop({ duration: 300 })}
-        >
-          <img src={logo} style={{ margin: 0, height: "70px" }} />
+    <StyledNav>
+      <div className="nav-container">
+        <span onClick={() => animateScroll.scrollToTop({ duration: 300 })}>
+          <img src={logo} />
         </span>
       </div>
-    </nav>
+    </StyledNav>
   )
 }
