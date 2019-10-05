f3f56c14ce345badbe0705fb8ccf4a77bba97472
Riley Brown <riley@riley.gg>
2019-10-05 18:51:58 -0400

    removed unneeded create page API

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,51 +8,51 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
   })
 }
 
-exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
-  return new Promise((resolve, reject) => {
-    const productTemplate = path.resolve("src/components/Product/index.js")
-    resolve(
-      graphql(`
-        {
-          allStripeSku {
-            edges {
-              node {
-                price
-                image
-                product {
-                  images
-                  description
-                  name
-                  metadata {
-                    slug
-                  }
-                }
-              }
-            }
-          }
-        }
-      `).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
+// exports.createPages = ({ graphql, boundActionCreators }) => {
+//   const { createPage } = boundActionCreators
+//   return new Promise((resolve, reject) => {
+//     const productTemplate = path.resolve("src/components/Product/index.js")
+//     resolve(
+//       graphql(`
+//         {
+//           allStripeSku {
+//             edges {
+//               node {
+//                 price
+//                 image
+//                 product {
+//                   images
+//                   description
+//                   name
+//                   metadata {
+//                     slug
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       `).then(result => {
+//         if (result.errors) {
+//           console.log(result.errors)
+//           reject(result.errors)
+//         }
 
-        result.data.allStripeSku.edges.forEach(edge => {
-          createPage({
-            path: edge.node.product.metadata.slug,
-            component: productTemplate,
-            context: {
-              slug: edge.node.product.metadata.slug,
-              itemName: edge.node.product.name,
-              itemPrice: edge.node.price,
-              itemDescription: edge.node.product.description,
-              itemImage: edge.node.image,
-            },
-          })
-        })
-        return
-      })
-    )
-  })
-}
+//         result.data.allStripeSku.edges.forEach(edge => {
+//           createPage({
+//             path: edge.node.product.metadata.slug,
+//             component: productTemplate,
+//             context: {
+//               slug: edge.node.product.metadata.slug,
+//               itemName: edge.node.product.name,
+//               itemPrice: edge.node.price,
+//               itemDescription: edge.node.product.description,
+//               itemImage: edge.node.image,
+//             },
+//           })
+//         })
+//         return
+//       })
+//     )
+//   })
+// }
